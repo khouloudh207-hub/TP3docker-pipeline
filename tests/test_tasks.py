5233d47b50681ea6f877840197e9460a9de31be8
khouloud <khouloudh207@gmail.com>
2026-03-24 18:18:59 +0100

    ajout du contenu au fichier test_tasks.py

---
+++ tests/test_tasks.py
@@ -0,0 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+def test_add_task():
+    add_task("Learn Docker")
+    assert "Learn Docker" in get_tasks()
+def test_multiple_tasks():
+    add_task("Learn CI")
+    add_task("Learn Devops")
+    tasks=get_tasks()
+    assert "Learn CI" in tasks
+    assert "Learn Devops" in tasks
+    
