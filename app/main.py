7cb855ddc1aa0789d5234b0bf9427e2d0f227dcd
khouloud <khouloudh207@gmail.com>
2026-03-29 17:08:13 +0100

    modifier le fichier main ex2tp3

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from flask import Flask, jsonify, request
 from tasks import add_task, get_tasks
+
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "API is running 🚀"
+
 @app.route('/tasks', methods=['GET'])
 def list_tasks():
     return jsonify(get_tasks())
+
 @app.route('/tasks', methods=['POST'])
 def create_task():
-    data=request.json
-    task=add_task(data['task'])
+    data = request.json
+    task = add_task(data['task'])
     return jsonify({'task': task}), 201
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
